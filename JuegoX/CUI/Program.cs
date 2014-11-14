c0fd66d42e5307af7ce4be6b4eb85cf546adc25a
juancatala <juanalfonso.catala@gmail.com>
2014-11-14 19:57:31 +0100

    a

---
+++ JuegoX/CUI/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JuanC.Programacion.Eval1.Juego.Logica;
+
+namespace JuanC.Programacion.Eval1.Juego.CUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int secreto = ModeloJuego.SecretoDeLaVida();
+            Console.WriteLine("El secreto de la vida es:" + secreto);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ JuegoX/Logica/Juego.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JuanC.Programacion.Eval1.Juego.Logica
+{
+    class ModeloJuego
+    {
+        public static int SecretoDeLaVida()
+        {
+            return 42;
+        }
+    }
+}
